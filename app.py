d015ec4b129980125a8073f938be0dea42a523a2
“Tobias” <tobi_m@live.dk>
2024-02-22 17:01:30 +0100

    adding ibm certificate

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 from streamlit_option_menu import option_menu
 from streamlit_lottie import st_lottie
+import streamlit.components.v1 as components
 
 
 st.set_page_config(
@@ -132,12 +133,12 @@ if selected == "Projects":
     st.header("Here's a couple of things I've worked on:")
     st.write("---")
 
+
     with st.container():
         col1, col2 = st.columns([3,1])
 
         with col1: 
-
-            # Project 1
+            # Project PDF to Content App
             st.subheader("📑 PDF to Content App")
             st.markdown("*This application allows users to upload a PDF file and then automatically get a Summary, 5 Question Multiple Choice Quiz and 10 Flashcards to study the uploaded content in different ways.*")
             st.markdown("The application was created to quickly start testing basic functionalities of a product idea two colleagues had")
@@ -157,18 +158,60 @@ if selected == "Projects":
             st.video(video_bytes)
 
     st.write("---") 
+
+    with st.container():
+        col7, col8 = st.columns([3,1])
+
+        with col7: 
+            # Project AI Journalist Assistant
+            st.subheader("🖋️ AI Journalist Assistant")
+            st.markdown("*AI Journalist Assistant is an innovative tool designed to assist content creators by automating the drafting process of articles. The application enables users to input their notes or upload a file, and using an OpenAI API, it generates a well-structured article.*")
+            st.markdown("Developed to enhance productivity, this application provides options for title style, tone, and formatting to tailor content to specific audiences or publications.")
+            st.markdown("The project streamlines the content creation workflow, offering features like downloading the article in a Word document and a text-to-speech function for auditory content review.")
+            st.markdown("**Tools Used:** Python, OpenAI API, Streamlit")
+            st.markdown("[Link to the project](https://ai-journalist.streamlit.app)")
+
+        with col8:
+            st_lottie("https://lottie.host/c8c17a6c-17fb-4cf4-b4b5-6336392c1904/a3J5jwyTcw.json",
+                      speed=0.8)
+            
+        with st.expander("🎥 Watch Demo", expanded=False):
+            video_file = open('videos/PDF_to_content_demo.mp4', 'rb')
+            video_bytes = video_file.read()
+            st.video(video_bytes)
+
+    st.write("---") 
+
+    with st.container():
+        col5, col6 = st.columns([3,1])
+
+        with col5:
+            #Project IBM Data Science Specialization Capstone Project
+            st.subheader("📊 IBM Data Science Specialization Capstone Project")
+            st.markdown("*This project was the final assignment for the IBM Data Science Specialization on Coursera.*")
+            st.markdown("The capstone project focuses on predicting the success of SpaceX Falcon 9 rocket first-stage landings using machine learning.")
+            st.markdown("It involves data collection through SpaceX API and web scraping, data wrangling, exploratory analysis using SQL and data visualization, interactive visual analytics, and machine learning prediction. ")
+            st.markdown("The goal is to identify key factors influencing rocket launch success rates and to develop a predictive model with high accuracy.")
+            st.markdown("**Tools Used:** Python, Pandas, Matplotlib, Plotly Dash, Folium, Scikit-learn")
+        with col6:
+            st_lottie("https://lottie.host/37def2a1-97e6-4bff-98f5-726c9c8971c0/tCPDv3ki5k.json",
+                      speed=0.6)
+
+        with st.expander("💻 Watch final presentation", expanded=False):
+            components.iframe("https://docs.google.com/presentation/d/e/2PACX-1vR6untpzpU4qLmnyqV1sPovR4hPoS9BhBjfo0dxpS04ICqkx1BSSKt4jMUMLL046Q7CXXk3Ey0kcP8F/embed?start=false&loop=false&delayms=3000", width=680, height=400)
+
+    st.write("---") 
     
     with st.container():
         col3, col4 = st.columns([3,1])
 
         with col3: 
-
-            # Project 2
+            # Project Diabetes Prediction App
             st.subheader("🔍 Diabetes Prediction App")
             st.markdown("*This tool provides a quick survey for users to complete. After analyzing your answers, it calculates the probability of you having diabetes using a prediction model trained with data from the National Health and Nutrition Examination Survey (NHANES).*")
             st.markdown("This application was created in collaboration with a group of students from the University of Michigan and the Michigan Data Science Team (MDST). While the model is not perfect, it is a good example of how data science can be used to create value for people.")
             st.markdown("*... if the data and questions asked are relevant.*")
-            st.markdown("**Tools Used:** Python, Pandas, Tensorflow, Streamlit")
+            st.markdown("**Tools Used:** Python, Pandas, Scikit-learn, Tensorflow, Streamlit")
             st.markdown("[Link to the project](https://mdst-diabetes-project.streamlit.app)")
 
         with col4:
